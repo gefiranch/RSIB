79d45f11b79b2ec9f489da6a05b344d9e0ca263a
gefiranch <Gefiranch@gmail.com>
2025-10-24 06:29:12 +0700

    Update config.php

---
+++ RSIB/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');      // ganti kalau username DB kamu beda
+define('DB_PASS', '');          // isi password DB kalau ada
+define('DB_NAME', 'sistem_pos');
+
+define('BASE_URL', 'http://localhost/SistemPOS');
+?>
