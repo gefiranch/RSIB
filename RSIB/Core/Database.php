8374bacae80785cdde867570f3e490da96ceff1b
gefiranch <Gefiranch@gmail.com>
2025-10-24 06:25:36 +0700

    Update Database.php

---
+++ RSIB/Core/Database.php
@@ -0,0 +1,44 @@
+<?php
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $db_name = DB_NAME;
+    private $dbh;
+    private $stmt;
+
+    public function __construct() {
+        try {
+            $this->dbh = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->user, $this->pass);
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Koneksi gagal: ' . $e->getMessage());
+        }
+    }
+
+    public function query($query) {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            $type = match (true) {
+                is_int($value) => PDO::PARAM_INT,
+                is_bool($value) => PDO::PARAM_BOOL,
+                is_null($value) => PDO::PARAM_NULL,
+                default => PDO::PARAM_STR,
+            };
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute() {
+        $this->stmt->execute();
+    }
+
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
